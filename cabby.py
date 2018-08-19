2ea0a5d2cc5129a072a071f0ac1a2ce6cb1c6621
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-08-19 17:03:28 +0530

    Assets modified

--- cabby.py
+++ cabby.py
@@ -1,15 +1,18 @@
 import pygame
 pygame.init()
+green=(0,255,0)
 gameDisplay=pygame.display.set_mode((800,600))
 pygame.display.set_caption('Cabby')
 gameOver=False
+carImg=pygame.image.load('./assets/car2.png')
 
 while not gameOver:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             gameOver=True
-    gameDisplay.fill((128,0,255))
+    gameDisplay.fill(green)
+    gameDisplay.blit(carImg,(400,200))
     pygame.display.flip() #updates the screen
-    
+
 pygame.quit()
 quit()
