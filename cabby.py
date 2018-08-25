8cc9e6f54341d233d1be1e2f8894e0097e52dc32
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-08-25 16:38:11 +0530

    Blocks going behind the car randomly

--- cabby.py
+++ cabby.py
@@ -1,15 +1,19 @@
 import pygame
 import obstacle
+import random
 pygame.init()
 clock=pygame.time.Clock()
 
 green=(0,255,0)
+DISPLAY_HEIGHT,DISPLAY_WIDTH=800,600
 gameDisplay=pygame.display.set_mode((800,600))
 pygame.display.set_caption('Cabby')
 gameOver=False
 carImg=pygame.image.load('./assets/car2.png')
 x,y=400,200
 x_change=0
+obstacle_x,obstacle_y=random.randint(0,DISPLAY_WIDTH),0
+obstacle_y_change=0
 while not gameOver:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -26,7 +30,14 @@ while not gameOver:
     gameDisplay.fill(green)
     x=x+x_change
     gameDisplay.blit(carImg,(x,y))
-    obstacle.obstacle(gameDisplay)
+
+    if obstacle_y_change>DISPLAY_HEIGHT:
+        obstacle_x=random.randint(0,DISPLAY_WIDTH)
+        obstacle_y_change=0
+        
+    obstacle.block(gameDisplay,color=(100,100,100),x=obstacle_x,y=obstacle_y+obstacle_y_change)
+    obstacle_y_change+=5
+    
     # pygame.draw.rect(gameDisplay,(0,0,0),[10,10,100,100])
     pygame.display.flip() #updates the screen
     clock.tick(60)
